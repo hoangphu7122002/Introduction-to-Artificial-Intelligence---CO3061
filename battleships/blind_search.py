916c8695ae31aae9359094d9155998bba102afda
tri218138 <80521604+tri218138@users.noreply.github.com>
2022-03-30 02:22:05 +0700

    Update blind_search.py

--- battleships/blind_search.py
+++ battleships/blind_search.py
@@ -1,5 +1,5 @@
-from battleships.gen_board import *
-from battleships.GUI import *
+from gen_board import *
+from GUI import *
 import numpy as np
 import copy
 from time import process_time
@@ -17,41 +17,45 @@ class DFS:
         self.dim = dim
         self.row_constraint = row_constraint
         self.col_constraint = col_constraint
-        self.ship = ship
+        self.ship = ship # ex: [3, 2, 2, 1, 1, 1]
         self.total_step = 0
-        self.solution = []
-        self.interupt = None
-        self.begin_time = process_time()
+        self.solution = None
+        self.interupt = None # signal Esc when GUI show
+        self.start_time = process_time()
         self.stop_time = 0
+    # search from column 0 to column 5
     def __search__(self, id_col):
-        # # debug
-        #print(self.board)
-        if (self.solution != []):# or self.interupt != None
+        # check solution exist
+        try:
+            if (self.solution != None):# or self.interupt != None
+                return
+        except:
             return
+        # GUI show
         prt = Gui(self.board,self.dim,self.row_constraint,self.col_constraint)
         self.stop_time = process_time()
-        prt.display(self.total_step, self.stop_time - self.begin_time,-1, 0.005)
+        prt.display(self.total_step, self.stop_time - self.start_time, 0.005)
+        # signal interupt by press Esc,...
         if (prt.interupt()):
             self.interupt = True
         self.total_step += 1
+        # END
         if (id_col == self.dim):
-            #accept
-            # print(self.board)
-            # n = int(input())
+            # prt = Gui(self.board,self.dim,self.row_constraint,self.col_constraint)
+            # prt.display(self.total_step, self.stop_time - self.start_time, 0)
             ok = self.check()
             if (ok):
-                self.solution.append(copy.deepcopy(self.board))
-                self.stop_time = process_time() - self.begin_time
+                # save solution board
+                self.solution = copy.deepcopy(self.board)
+                self.stop_time = process_time() - self.start_time
             return
+        # calculate number of BLOCK in each column need to put down
         remain = self.col_constraint[id_col] - np.count_nonzero(self.board[:,id_col])
-        # # debug
-        # print(self.board[:,id_col])
-        # print("column " + str(id_col) +" remain = " + str(remain))
-        keep = []
+        # if number == 0
         if (remain <= 0):
             self.__search__(id_col + 1)
             return
-
+        # else shuffle this column
         for t in range(0, self.dim):
             keep = []
             tmp = copy.deepcopy(remain)
@@ -69,34 +73,39 @@ class DFS:
                         tmp += 1 
                         keep.pop()
     def solve(self):
-        # random according to col_constraint
         # start from id_col 0
         self.__search__(0)
         # display solution
-        if (self.solution != []):           
-            print("===== SOLUTION =====")
-            init = Gui(self.prtSolutionBoard,self.dim,self.row_constraint,self.col_constraint)
-            init.display(self.total_step, self.stop_time,-1)
-            vis = np.zeros((self.dim, self.dim))
-            for i in range(0, self.dim):
-                for j in range(0, self.dim):
-                    if (self.solution[0][i][j] == BLOCK and vis[i][j] == 0):
-                        ii = copy.deepcopy(i)
-                        jj = copy.deepcopy(j)
-                        while (ii in range(0, self.dim) and self.solution[0][ii][jj] == BLOCK):
-                            self.prtSolutionBoard[ii][jj] = BLOCK
-                            vis[ii][jj] = 1
-                            ii += 1
-                        ii = copy.deepcopy(i)
-                        jj = copy.deepcopy(j)
-                        while (jj in range(0, self.dim) and self.solution[0][ii][jj] == BLOCK):
-                            self.prtSolutionBoard[ii][jj] = BLOCK
-                            vis[ii][jj] = 1
-                            jj += 1
-                        gui_board = Gui(self.prtSolutionBoard,self.dim,self.row_constraint,self.col_constraint)
-                        gui_board.display(self.total_step, self.stop_time)
-            gui_board = Gui(self.prtSolutionBoard,self.dim,self.row_constraint,self.col_constraint)
-            gui_board.display(self.total_step, self.stop_time,-1)
+        try:
+            if (self.solution != None):           
+                print("===== SOLUTION =====")
+                init = Gui(self.prtSolutionBoard,self.dim,self.row_constraint,self.col_constraint)
+                init.display(self.total_step, self.stop_time)
+                vis = np.zeros((self.dim, self.dim))
+                for i in range(0, self.dim):
+                    for j in range(0, self.dim):
+                        if (self.solution[i][j] == BLOCK and vis[i][j] == 0):
+                            ii = copy.deepcopy(i)
+                            jj = copy.deepcopy(j)
+                            while (ii in range(0, self.dim) and self.solution[ii][jj] == BLOCK):
+                                self.prtSolutionBoard[ii][jj] = BLOCK
+                                vis[ii][jj] = 1
+                                ii += 1
+                            ii = copy.deepcopy(i)
+                            jj = copy.deepcopy(j)
+                            while (jj in range(0, self.dim) and self.solution[ii][jj] == BLOCK):
+                                self.prtSolutionBoard[ii][jj] = BLOCK
+                                vis[ii][jj] = 1
+                                jj += 1
+                            gui_board = Gui(self.prtSolutionBoard,self.dim,self.row_constraint,self.col_constraint)
+                            gui_board.display(self.total_step, self.stop_time)
+                gui_board = Gui(self.prtSolutionBoard,self.dim,self.row_constraint,self.col_constraint)
+                gui_board.display(self.total_step, self.stop_time, 0)
+        except:
+            # in case of break while searching the solution
+            print("No solution found yet!")
+######## HELPER FUNCTION ########
+    ### get border of the ship ###
     def get_border(self, ship):
         border = []
         for sh in range(0, len(ship)):
@@ -105,6 +114,7 @@ class DFS:
                 if (not list(neigh[ne]) in ship and not neigh[ne] in border):
                     border.append(neigh[ne])
         return border
+    ### check this board can be a solution ? ###
     def check(self):
         # not over constraint
         for i in range(self.dim):
@@ -167,32 +177,33 @@ class DFS:
             if (type_ship[i] != self.ship[i]):
                 return False
         return True
+    # using to show one of the solution
     def show(self):
-        if (self.solution != []):
-            print_board(self.solution[0], self.dim, self.col_constraint, self.row_constraint)
+        if (self.solution != None):
+            print_board(self.solution, self.dim, self.col_constraint, self.row_constraint)
         else:
             print("No solution found!")
     def get_total_step_search(self):
         return self.total_step
 
-######## debug ##########
-
-# if __name__ == '__main__':
-#     dim = 6
-#     gen_object = gen_board(dim)
-#     board = gen_object.get_board()
-#     row_constraint = gen_object.get_row_constraint()
-#     col_constraint = gen_object.get_col_constraint()
-#     solutionBoard = gen_object.get_solution()
+if __name__ == '__main__':
+    dim = 6
+    gen_object = gen_board(dim)
+    board = gen_object.get_board()
+    row_constraint = gen_object.get_row_constraint()
+    col_constraint = gen_object.get_col_constraint()
+    solutionBoard = gen_object.get_solution()
     
-#     print("\n==============GENERATE================\n")
-#     print_board(board,dim,col_constraint,row_constraint)
-#     print("\n==============SOLUTION================\n")
-#     print_board(solutionBoard,dim,col_constraint,row_constraint)
-#     print("\n======================================\n")
+    print("\n==============GENERATE================\n")
+    print_board(board,dim,col_constraint,row_constraint)
+    print("\n======================================\n")
 
-#     dfs = DFS(gen_object.get_board(), gen_object.get_ship(), gen_object.get_row_constraint(), gen_object.get_col_constraint(), dim)
-#     dfs.solve()
-#     dfs.show()
-#     print("Total step search " +str(dfs.get_total_step_search()))
-    
+    print("\n==============SOLUTION================\n")
+    print_board(solutionBoard,dim,col_constraint,row_constraint)
+    print("\n======================================\n")
+
+    # DFS algorithm
+    dfs = DFS(gen_object.get_board(), gen_object.get_ship(), gen_object.get_row_constraint(), gen_object.get_col_constraint(), dim)
+    dfs.solve()
+    dfs.show()
+    print("Total step search " +str(dfs.get_total_step_search()))
