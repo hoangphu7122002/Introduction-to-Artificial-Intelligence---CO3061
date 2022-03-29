e5dd8e1cb2e1804d4749fe9af7dc9d5fc70473ea
tri218138 <80521604+tri218138@users.noreply.github.com>
2022-03-29 14:48:02 +0700

    Update geneticAI.py
    GUI

--- debug/battleships/geneticAI.py
+++ debug/battleships/geneticAI.py
@@ -2,10 +2,14 @@ from gen_board import *
 import time
 import numpy as np
 from time import process_time
+from GUI import *
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 class Genetic:
     def __init__(self, board, ship, row_constraint, col_constraint, dim = 6):
         self.board = np.array(board) #numpy array
+        self.prtSolutionBoard = np.array(board)
         self.dim = dim
         self.row_constraint = row_constraint
         self.col_constraint = col_constraint
@@ -17,6 +21,7 @@ class Genetic:
         self.interupt = None
         self.begin_time = process_time()
         self.stop_time = 0
+        self.population = 50
     def __search__(self, board):
         pass
     def get_border(self, ship):
@@ -108,22 +113,29 @@ class Genetic:
         for c in range(self.dim):
             cnt += np.count_nonzero(state[:, c]) == self.col_constraint[c] 
         return cnt
-    def fitness_comp(self, state):
-        return self.fitness(state) 
     def solve(self):
-        num_of_populations = 50
-        done = False
+        num_of_populations = self.population
         for i in range(num_of_populations):
             self.gen.append(self.generate_board())
-        self.gen.sort(key = self.fitness_comp, reverse = True)
-        ans = None
+        self.gen.sort(key = self.fitness, reverse = True)
         self.generation = 0
         # getting started generation
-        while not done:
+        # pie chart
+        fig, ax = plt.subplots()
+        def generate(num):
+            try:
+                if (self.solution != None):
+                    ax.set_title("Generation " + str(self.generation) + "\nSolution found!\nClose the window to view solution")
+                    ax.pie(percen_plt, labels = mylabels, startangle = 90)
+                    return
+            except:
+                return
+            ax.clear()
+            ax.axis('equal')
             # if (self.solution is not None):# or self.interupt != None
             #     return
-            print("Generation " + str(self.generation))
             self.generation += 1
+            ax.set_title("Generation " + str(self.generation))
             # selections, random 2 index, choice better
             # for i in range(0, num_of_populations):
             #     inx1 = np.random.randint(0, num_of_populations)
@@ -163,22 +175,56 @@ class Genetic:
                 self.mutate(self.gen[inx])   
             #=========#
             #print and check
+            percen = np.zeros(self.dim + 1)
             for i in range(0, num_of_populations):
-                print(str(self.fitness(self.gen[i])), end=' ')      
+                tmp = self.fitness(self.gen[i])
+                percen[tmp] += 1
+                # print(str(tmp), end=' ')      
                 if (self.check(self.gen[i])):
                     #print_board(self.gen[i + 1],self.dim,self.col_constraint,self.row_constraint)
                     self.solution = copy.deepcopy(self.gen[i])
                     self.stop_time = process_time() - self.begin_time
-                    done = True
-                    break  
-            print('\n') 
+                    # done = True
+            # print('\n') 
+            percen_plt = np.array(percen)
+            mylabels = [str(i) for i in range(self.dim + 1)]
+            ax.pie(percen_plt, labels = mylabels, startangle = 90)
             #=========#
             # debug pause       
             # if (generation % 50 == 0):
             #     for i in range(0, num_of_populations):
             #         print_board(self.gen[i],self.dim,self.col_constraint,self.row_constraint)
-            #     n = input()
-
+            #     n = input() 
+        ani = FuncAnimation(fig, generate, frames=range(500), repeat=False)
+        plt.show()
+        try:
+            if (self.solution == None):
+                print("No solution found yet!")
+        except:
+            plt.close('all')
+            print("===== SOLUTION =====")
+            init = Gui(self.prtSolutionBoard,self.dim,self.row_constraint,self.col_constraint)
+            init.display_AI(self.generation, self.stop_time)
+            vis = np.zeros((self.dim, self.dim))
+            for i in range(0, self.dim):
+                for j in range(0, self.dim):
+                    if (self.solution[i][j] == BLOCK and vis[i][j] == 0):
+                        ii = copy.deepcopy(i)
+                        jj = copy.deepcopy(j)
+                        while (ii in range(0, self.dim) and self.solution[ii][jj] == BLOCK):
+                            self.prtSolutionBoard[ii][jj] = BLOCK
+                            vis[ii][jj] = 1
+                            ii += 1
+                        ii = copy.deepcopy(i)
+                        jj = copy.deepcopy(j)
+                        while (jj in range(0, self.dim) and self.solution[ii][jj] == BLOCK):
+                            self.prtSolutionBoard[ii][jj] = BLOCK
+                            vis[ii][jj] = 1
+                            jj += 1
+                        gui_board = Gui(self.prtSolutionBoard,self.dim,self.row_constraint,self.col_constraint)
+                        gui_board.display_AI(self.generation, self.stop_time)
+            gui_board = Gui(self.prtSolutionBoard,self.dim,self.row_constraint,self.col_constraint)
+            gui_board.display_AI(self.generation, self.stop_time, 0)
     def mutate(self, state):
         #cur_fitness = self.fitness(state)   
         num_mutate_row = np.random.randint(0, self.dim, np.random.randint(0, self.dim) + 1)
